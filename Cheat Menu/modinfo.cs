3439f14472a016fa242b331e72092b6c37a043a0
joey549 <50384934+joey549@users.noreply.github.com>
2025-06-15 19:32:43 -0400

    Update modinfo.cs
    Updated some values

--- Cheat Menu/modinfo.cs
+++ Cheat Menu/modinfo.cs
@@ -9,9 +9,9 @@ namespace Modern_Cheat_Menu
    public static class ModInfo
     {
         public const string Name = "Modern Cheat Menu";
-        public const string Version = "2.0.2";
+        public const string Version = "2.1.0";
         public const string Author = "darkness";
-        public const string Description = "An open source mod menu for Schedule I that employs powerful features, leveraging various game mechanics and routines.";
+        public const string Description = "A forked mod menu developed by tcphdr for Schedule I that implements the developer console commands and more. Edited By:joey549(SarkastikDemon)";
         public const string RepositoryUrl = "https://github.com/tcphdr/modern-cheat-menu";
         public const string NameOfGame = "Schedule I";
         public const string GameDevelopers = "TVGS";
